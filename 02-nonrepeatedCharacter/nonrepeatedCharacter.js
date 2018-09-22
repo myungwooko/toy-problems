57eb85e29e35090c117c4127c653cb28ca5bfedc
pre20myungwoo <myungwoo.ko@gmail.com>
2018-09-23 00:38:27 +0900

    myungwoo toy02 completed

--- 02-nonrepeatedCharacter/nonrepeatedCharacter.js
+++ 02-nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,23 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  var str = string.toUpperCase();
+  var obj = {};
+  for(var i = 0; i < str.length; i++) {
+    if(!obj[str[i]]) {
+      obj[str[i]] = 1;
+    } else {
+      obj[str[i]]++;
+    }
+  }
+  for(var key in obj) {
+    if(obj[key]===1) {
+      return key;
+    }
+  }
 };
+
+var a = firstNonRepeatedCharacter('ABA'); // => 'B'
+var b = firstNonRepeatedCharacter('AACBDB'); // => 'C'
+console.log(a)
+console.log(b)

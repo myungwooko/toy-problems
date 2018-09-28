86c992112851f18229b2e4b8a5d56f978aa39362
pre20myungwoo <myungwoo.ko@gmail.com>
2018-09-28 11:47:42 +0900

    myungwoo toy09 completed

--- 09-deepEquality/deepEquality.js
+++ 09-deepEquality/deepEquality.js
@@ -11,5 +11,49 @@
   * don't worry about handling cyclical object structures.
   *
   */
+
+//The easiest way
+// var deepEquals = function(apple, orange){
+//   var apple  = JSON.stringify(apple);
+//   var orange = JSON.stringify(orange);
+//   if(apple === orange) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+
+//Another way using recursion
 var deepEquals = function(apple, orange){
+  return recursion(apple, orange)
 };
+
+function recursion(obj1, obj2) {
+  var key1 = Object.keys(obj1);
+  var key2 = Object.keys(obj2);
+  if(key1.length !== key2.length) {
+    return false;
+  } 
+  for(var i = 0; i < key1.length; i++) {
+    if(typeof obj1[key1[i]] === 'object' && typeof obj2[key2[i]] === 'object') {
+      return recursion(obj1[key1[i]], obj2[key2[i]])
+    } else {
+      if(obj1[key1[i]] !== obj2[key2[i]]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+
+var a = deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
+var b = deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
+var c = deepEquals({a:1, b: {c: {d: 3}}},{a:1, b: {c: {d: 3}}}); // true
+var d = deepEquals({a:1, b: {c: {d: 3}}},{a:1, b: {c: {e: 6}}}); // false
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)

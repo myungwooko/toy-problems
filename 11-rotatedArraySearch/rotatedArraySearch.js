76da98b191c2b766bd00596fd1dc9d6f7a0346f5
pre20myungwoo <myungwoo.ko@gmail.com>
2018-09-30 23:49:14 +0900

    myungwoon toy11 completed

--- 11-rotatedArraySearch/rotatedArraySearch.js
+++ 11-rotatedArraySearch/rotatedArraySearch.js
@@ -17,5 +17,33 @@
 
 var rotatedArraySearch = function (rotated, target) {
   // Your code here:
+  function recursion(accumulated, array) {
+    var theCenter = Math.floor(array.length/2)
+    if(array.length === 1) {
+      if(array[0] === target) {
+        return accumulated;
+      } else {
+        return null;
+      }
+    }
+    for(var i = 0; i < theCenter; i++) {
+      if(array[i] === target) {
+        return accumulated + i;
+      } else {
+        array = array.splice(theCenter, array.length);
+        accumulated += theCenter
+        return recursion(accumulated, array)
+      }
+    }
+  }
+  return recursion(0, rotated)
 };
 
+var a = rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 2)   // === 5
+var b = rotatedArraySearch([4, 5, 6, 0, 1, 2, 3], 100) // === null
+var c = rotatedArraySearch([4, 7, 1, 2, 3, 11], 11)    // === 5
+var d = rotatedArraySearch([4, 7, 1, 2, 3, 11], 123)   // === null
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)

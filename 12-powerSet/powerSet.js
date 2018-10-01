29f4489c77db1c54493c45266e498e1fea93a354
pre20myungwoo <myungwoo.ko@gmail.com>
2018-10-01 14:37:05 +0900

    myungwoo toy12 completed

--- 12-powerSet/powerSet.js
+++ 12-powerSet/powerSet.js
@@ -18,4 +18,46 @@
  */
 
 var powerSet = function(str){
-}
+    var arr = str.split('')
+    var originModel = arr.sort().slice();
+    var result = arr.sort().slice();
+
+    function recursion(array, num, startPoint) {
+        if(num > str.length) {
+            return; 
+        }
+        var beforeLength = array.length;
+        for(var i = startPoint; i < array.length; i++) {
+            for(var z = 0; z < originModel.length; z++) {
+                var ele = (array[i] + originModel[z]).split('').sort().join('')
+                if(checkForSame(ele) && array.indexOf(ele) === -1) {
+                    array.push(ele);
+                }
+            }
+        }
+        var afterLength = array.length;
+        var nextStartPoint = afterLength - beforeLength;
+        recursion(array, num+1, nextStartPoint)
+    };
+    
+    recursion(result, 2, 0)
+    var result = [''].concat(result).sort()
+    return result
+};
+
+function checkForSame(str) {
+    var arr = str.split('')
+    for(var i = 0; i < arr.length - 1; i++) {
+        for(var z = i+1; z < arr.length; z++) {
+            if(arr[i] === arr[z]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+var a = powerSet("jump");
+console.log(a)
+var b = powerSet("abc");
+console.log(b)

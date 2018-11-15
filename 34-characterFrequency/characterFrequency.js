347afd6595be20968ea09bacad87a13d81ddcd84
pre20myungwoo <myungwoo.ko@gmail.com>
2018-11-15 11:58:21 +0900

    myungwoo toy34 completed

--- 34-characterFrequency/characterFrequency.js
+++ 34-characterFrequency/characterFrequency.js
@@ -1,7 +1,7 @@
 /*
  *  Write a function that takes as its input a string and returns an array of
  *  arrays as shown below sorted in descending order by frequency and then by
- *  ascending order by character.
+ *  ascending order by character. (*number is first)
  *
  *       :: Example ::
  *
@@ -36,6 +36,44 @@
  */
 
 
-var characterFrequency = function(string) {
-  return result;
+var characterFrequency = function (string) {
+  var obj = {}
+  var result = [];
+  var alp = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  for (var i = 0; i < string.length; i++) {
+    if (!obj[string[i]]) {
+      obj[string[i]] = 1;
+    } else {
+      obj[string[i]]++;
+    }
+  }
+  for (var key in obj) {
+    result.push([key, obj[key]])
+  }
+  result.sort(function (a, b) {
+    return b[1] - a[1]
+  })
+  var sorting = (arr) => {
+    var count = 0
+    var tmp;
+    for (var i = 0; i < arr.length - 1; i++) {
+      if (arr[i][1] === arr[i + 1][1]) {
+        if (alp.indexOf(arr[i][0]) > alp.indexOf(arr[i + 1][0])) {
+          tmp = arr[i][0]
+          arr[i][0] = arr[i + 1][0]
+          arr[i + 1][0] = tmp
+          count++
+        }
+      }
+    }
+    if (count !== 0) {
+      return sorting(arr)
+    } else {
+      return arr
+    }
+  }
+  return sorting(result)
 };
+
+var a = characterFrequency('mmmaaaiiibbb')
+console.log(a)

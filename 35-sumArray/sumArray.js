1857a3c8f5d7541f8d62064df3e177eec9802403
pre20myungwoo <myungwoo.ko@gmail.com>
2018-11-19 10:48:09 +0900

    myungwoo toy35 completed

--- 35-sumArray/sumArray.js
+++ 35-sumArray/sumArray.js
@@ -9,6 +9,25 @@
  * example 5: sumArray([10, -11, 11]); // 11
  */
 
-// Solved in O(n) time with O(1) memory
-var sumArray = function(array) {
+// // Solved in O(n) time with O(1) memory
+
+var sumArray = array => {
+  let sum = 0;
+  let result = array[0];
+  array.forEach(item => {
+    sum += item;
+    if (sum >= result) {
+      result = sum;
+    }
+    if (sum < 0) {
+      sum = 0;
+    }
+  });
+  return result;
 };
+
+console.log(sumArray([10, 1, 4, 5, -1, 1000, 3])); // => 1022
+console.log(sumArray([1, 2, 3, -4])); // 6
+console.log(sumArray([1, 2, 3, -4, 5])); // 7
+console.log(sumArray([4, -1, 5])); // => 8
+console.log(sumArray([10, -11, 11])); // 11

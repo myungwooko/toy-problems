3bef3d63a886ffc5003676d8fb846cc8dfe5aced
pre20myungwoo <myungwoo.ko@gmail.com>
2018-11-23 15:30:29 +0900

    myungwoo toy38 completed

--- 38-telephoneWords/telephoneWords.js
+++ 38-telephoneWords/telephoneWords.js
@@ -1,31 +1,31 @@
 /*
-  * Each number key on a standard phone keypad has a set of Latin letters written on
-  * it as well: http://en.wikipedia.org/wiki/File:Telephone-keypad2.svg
-  *
-  * Businesses often try to come up with clever ways to spell out their phone number
-  * in advertisements to make it more memorable. But there are a lot of combinations!
-  *
-  * Write a function that takes up to four digits of a phone number, and
-  * returns a list of all of the words that can be written on the phone with
-  * that number. (You should return all permutations, not only English words.)
-  *
-  * Example:
-  *   telephoneWords('2745');
-  *   => ['APGJ',
-  *        'APGK',
-  *        'APGL',
-  *        ..., // many many more of these
-  *        'CSIL']
-  *
-  * Tips:
-  *   - Phone numbers are strings! (A phone number can start with a zero.)
-  *   - The digits 0 and 1 do not have letters associated with them, so they should be left as numbers.
-  *   - Don't return every combination of those digits in any order, just the order given.
-  *
-  *  Extra credit: There's a list of English dictionary words at /usr/share/dict/words .
-  *  Why not filter your results to only return words contained in that file?
-  *
-  */
+ * Each number key on a standard phone keypad has a set of Latin letters written on
+ * it as well: http://en.wikipedia.org/wiki/File:Telephone-keypad2.svg
+ *
+ * Businesses often try to come up with clever ways to spell out their phone number
+ * in advertisements to make it more memorable. But there are a lot of combinations!
+ *
+ * Write a function that takes up to four digits of a phone number, and
+ * returns a list of all of the words that can be written on the phone with
+ * that number. (You should return all permutations, not only English words.)
+ *
+ * Example:
+ *   telephoneWords('2745');
+ *   => ['APGJ',
+ *        'APGK',
+ *        'APGL',
+ *        ..., // many many more of these
+ *        'CSIL']
+ *
+ * Tips:
+ *   - Phone numbers are strings! (A phone number can start with a zero.)
+ *   - The digits 0 and 1 do not have letters associated with them, so they should be left as numbers.
+ *   - Don't return every combination of those digits in any order, just the order given.
+ *
+ *  Extra credit: There's a list of English dictionary words at /usr/share/dict/words .
+ *  Why not filter your results to only return words contained in that file?
+ *
+ */
 
 var phoneDigitsToLetters = {
   0: '0',
@@ -41,6 +41,28 @@ var phoneDigitsToLetters = {
 };
 
 
-var telephoneWords = function(digitString) {
+var telephoneWords = function (digitString) {
   // TODO: return every combination that can be spelled on a phone with these digits
+  var result = []
+  var recursion = (num, strSum) => {
+    if (num === digitString.length) {
+      result.push(strSum);
+      return;
+    }
+    var turnNumbersDigit = phoneDigitsToLetters[digitString[num]]
+    for (var i = 0; i < turnNumbersDigit.length; i++) {
+      recursion(num + 1, strSum + turnNumbersDigit[i])
+    }
+  }
+  recursion(0, '')
+  return result;
 };
+
+var a = telephoneWords('2745');
+console.log(a)
+
+// => ['APGJ',
+//      'APGK',
+//      'APGL',
+//      ..., // many many more of these
+//      'CSIL']
